0eec100f787a5748d01eee56d8b197e3cadb6bde
SeasonedPorks <yanawarood.yk@gmail.com>
2022-09-09 07:07:49 +0700

    add Bean somehow broke

--- src/main/java/se331com/rest/Lab082Application.java
+++ src/main/java/se331com/rest/Lab082Application.java
@@ -2,8 +2,19 @@ package se331com.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+ @SpringBootApplication
+ @Bean
+public WebMvcConfigurer corsConfigurer(){
+     return new WebMvcConfigurer() {
+         @Override
+public void addCorsMappings(CorsRegistry registry){
+             registry.addMapping("/event")
+        .allowedOrigins("http://localhost:3000");
+        }
+     }
+ }
 public class Lab082Application {
 
     public static void main(String[] args) {
